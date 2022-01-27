a9c0982150acaf8c3e1c2267e25079cdb1f7c79c
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-01-27 17:27:26 -0300

    One day

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/my_app.dart';
+
+void main(){
+  runApp(MyApp());
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
