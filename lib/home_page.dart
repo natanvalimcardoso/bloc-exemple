7c4e8a5f089e31b0e9cb6d3c360f979c7965304f
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2022-01-28 16:12:20 -0300

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,68 @@
-import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class HomePage extends State<TodoList> {
+  final List<String> _todoList = <String>[];
+  final TextEditingController _textFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: ListView(children: _getItems()),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
     );
+  }
+
+  void _addTodoItem(String title) {
+    // Wrapping it inside a set state will notify
+    // the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+  // Generate list of item widgets
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+
+  // Generate a single item widget
+  Future<AlertDialog> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a task to your list'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: 'Enter task here'),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('ADD'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              FlatButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
   }
 }
